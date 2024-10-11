6d7ad7c8fd882d11db9cb51fd0d6027934a65c26
Sergei12677 <gorod_26@icloud.com>
2024-10-11 23:23:27 +0500

    Add files via upload

---
+++ mobile_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Sergei':'1977'}
+my_dict ['Sergei'] = 2024
+my_dict ['Антон'] = 2023
+my_dict ['Вася'] = 2025
+print(my_dict)
+del my_dict ['Антон']
+print(my_dict)
+my_set = {1,2,3,4,'Тыква',1,2,3,}
+print(my_set)
+my_set = {1,2,3,'String',True,(1,2,3)}
+print(my_set)
+List_ = [1,2,3,4,5]
+list_ ='my_set','list_'
+print(list_)
